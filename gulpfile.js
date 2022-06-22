74b1974acc8fb1c34ebeeab99c0a72ac0b3f9c19
Lu-X-Liu <91901553+Lu-X-Liu@users.noreply.github.com>
2022-06-21 21:36:10 -0500

    initialize gulpfile.js

--- gulpfile.js
+++ gulpfile.js
@@ -1,10 +1,64 @@
 // list dependences
-
+const { src, dest, watch, series } = require('gulp');
+const sass = require('gulp-sass');
+const prefix = require('gulp-autoprefixer');
+const cleanCss = require('gulp-clean-css');
+const terser = require('gulp-terser');
+const imagemin = require('gulp-imagemin');
+const webp = require('gulp-webp');
+const rename = require('gulp-rename');
+const browserSync = require('browser-sync').create();
 
 //create functions
-
-
-//create watchtask
-
+//scss
+function scssTask() {
+    return src('src/scss/*.scss', {sourcemaps: true})
+      .pipe(sass().on('error', sass.logError))
+      .pipe(prefix('last 2 versions'))
+      .pipe(cleanCss())
+      .pipe(dest('dist/css', {sourcemaps: '.'}))
+      .pipe(browserSync.stream());
+}
+//js
+function js() {
+    return src('src/js/*.js', {sourcemaps: true})
+      .pipe(terser())
+      .pipe(rename({extname: '.min.js'}))
+      .pipe(dest('dist/js', {sourcemaps: '.'}))
+      .pipe(browserSync.stream());
+}
+// optimize images
+function optimizeImgs() {
+    return src('src/imgs/**/*.{jpg,png,svg}')
+      .pipe(imagemin([
+        imagemin.mozjpeg({quality:80, progressive: true}),
+        imagemin.optipng({optimizationLevel: 2})
+      ]))
+      .pipe(dest('dist/imgs'));
+}
+//create webp
+function webpImgs() {
+    return src('dist/imgs/**/*/{jpg,png}')
+    .pipe(webp())
+    .pipe(dest('dist/imgs'));
+}
+//watchtask
+function watchTask() {
+    browserSync.init({
+        server: {
+            baseDir: './'
+        }
+    });
+    watch('./src/scss/**/*.scss', scssTask);
+    watch('./src/*.html').on('change', browserSync.reload);
+    watch('./src/js/**/*.js', js);
+    watch('./src/imgs/**/*.{jpg,png.svg}', optimizeImgs);
+    watch('./dist/imgs/**/*.{jpg,png.svg}', webpImgs);
+}
 
 //default gulp
+exports.default = series(
+    scssTask,
+    js,
+    watchTask
+);

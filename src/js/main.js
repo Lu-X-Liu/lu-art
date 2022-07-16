3cca239984d4e2d4b773fe3014fda00d5873c37e
Lu-X-Liu <91901553+Lu-X-Liu@users.noreply.github.com>
2022-07-15 21:55:28 -0500

    js

--- src/js/main.js
+++ src/js/main.js
@@ -1,7 +1,22 @@
+//header variables
 const menuIcon = document.querySelector('.menu');
 const navList = document.querySelector('.nav__list');
 const navLinks = document.querySelectorAll('.nav__link');
 
+//thumbnail grid and light box variables
+const thumbImgs = document.querySelectorAll('.thumbnails__img');
+const thumbImgsArr = Array.from(thumbImgs);
+const lastImgIndex = thumbImgsArr.length - 1;
+
+const lightBox = document.querySelector('.light-box');
+const displayImg = document.querySelector('.light-box__img-display');
+const closeIcon = document.querySelector('.display__cross-icon');
+const arrowBtns = document.querySelectorAll('.display-btn__arrows');
+const leftArrow = document.querySelector('#left');
+const rightArrow = document.querySelector('#right');
+let activeImgIndex;
+
+//header event listeners
 menuIcon.addEventListener('click', () => {
     menuIcon.classList.toggle('menu--active');
     navList.classList.toggle('nav__list--active');
@@ -13,3 +28,62 @@ navLinks.forEach(link => {
         navList.classList.remove('nav__list--active');
     })
 })
+
+//thumbnail and light box functions
+function showLightBox() {
+    lightBox.classList.add('active');
+};
+
+function hideLightBox() {
+    lightBox.classList.remove('active');
+};
+
+function setActiveImg(img) {
+    displayImg.srcset = img.dataset.imgSrcset;
+    displayImg.src = img.dataset.imgSrc;  
+    displayImg.alt = img.alt.replace(' Thumbnail Image', '');
+    
+    activeImgIndex = thumbImgsArr.indexOf(img);
+};
+
+function unsetActiveImg() {
+    displayImg.srcset = '';
+    displayImg.src = ''; 
+    displayImg.alt = '';   
+};
+
+function transitionSlideLeft() {
+    leftArrow.focus();
+    activeImgIndex === 0 ? setActiveImg(thumbImgsArr[lastImgIndex]) :
+    setActiveImg(thumbImgsArr[activeImgIndex - 1]);
+};
+
+function transitionSlideRight() {
+    rightArrow.focus();
+    activeImgIndex === lastImgIndex ? setActiveImg(thumbImgsArr[0]) :
+    setActiveImg(thumbImgsArr[activeImgIndex + 1]);
+};
+
+function transitionSlideHandler(arrowId) {
+    arrowId.includes('left') ? transitionSlideLeft() : transitionSlideRight();
+};
+
+//thumbnail and light box event listeners
+thumbImgs.forEach(img => {
+    img.addEventListener('click', (e) => {
+        showLightBox();
+        setActiveImg(img);
+    })
+});
+
+closeIcon.addEventListener('click', ()=> {
+    hideLightBox();
+    unsetActiveImg();
+});
+
+arrowBtns.forEach(btn => {
+    btn.addEventListener('click', (e) => {
+        transitionSlideHandler(e.currentTarget.id)
+    })
+});
+
